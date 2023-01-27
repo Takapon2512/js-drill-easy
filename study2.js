971aac6a2487ebb7ecaaae293f22f8a84acb89e7
たかぽん <takapontype0924@gmail.com>
2023-01-27 16:34:02 +0900

    230127-1633_study2.js

---
+++ study2.js
@@ -0,0 +1,18 @@
+//複数のオブジェクト要素を持つ配列の定義
+const object = [
+    {id: 1, name: 'ジョン', age: 15},
+    {id: 2, name: 'ボブ', age: 20},
+    {id: 3, name: 'マイケル', age: 15}
+]
+console.log(object)
+
+//配列オブジェクト複数のオブジェクト要素を持つ配列からオブジェクト取得
+console.log(object[1]);
+
+//複数のオブジェクト要素を持つ配列からプロパティの値取得
+console.log(object[2].age)
+
+//配列をコピーして、新しい配列を作成しよう
+
+
+
